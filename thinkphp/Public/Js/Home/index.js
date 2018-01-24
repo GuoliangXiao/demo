5dd0dd666d2adc4bf745a3b2f6b02fc59175c86a
806564306 <806564306@qq.com>
2018-01-24 11:22:52 +0800

    add d3svg

--- thinkphp/Public/Js/Home/index.js
+++ thinkphp/Public/Js/Home/index.js
@@ -4,9 +4,10 @@
 jQuery(document).ready(function($) {
 	$(".slippry-ppt").slippry();
 	$(".slippry-two").particleground();
-	setCalendar();			
+	setCalendar();	
 });
 function setCalendar(){
 	var cal=new SimpleCalendar(".my-date");
 	cal.updateSize("20em","19em");
 }
+
