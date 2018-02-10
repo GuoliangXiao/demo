da3180b1d6046bd5e668adf8ede6db01831fe4e6
806564306 <806564306@qq.com>
2018-02-10 22:38:39 +0800

    v

--- thinkphp/Application/Runtime/Cache/Apps/6059f884a5c0418f94af3a45d93c2062.php
+++ thinkphp/Application/Runtime/Cache/Apps/6059f884a5c0418f94af3a45d93c2062.php
@@ -54,7 +54,7 @@
 			callback();
     	}, {type: type, confirmButtonText: ok});
 	}
-	function confirm_alt(txt,callback1, callback2, title="",type="info",ok="确定", cancel="取消"){
+	function confirm_alt(title="",txt,callback1, callback2,type="info",ok="确定", cancel="取消"){
 		confirm(title, txt , function (isConfirm) {
             if (isConfirm) {
                 callback1();
@@ -63,65 +63,7 @@
             }
         }, {confirmButtonText: ok, cancelButtonText: cancel, width: 400});
 	}
-</script><body style="background: #eeeeee;"><style type="text/css">
-	.mynavdiv{
-		padding: 0;
-		margin: 0;
-	}
-	.mynav{
-		background: #2A2730;
-		margin-top:0em;
-		padding:0.3em;
-		width: 100%;
-	}
-	.mynav>li{
-		width: 7em;
-		padding: 0;
-		margin: 0;
-	}
-	.mynav>li:hover{
-		background:rgba(255,255,255,0.5);
-	}
-	.mynav>li>a{
-		font-size: 1.2em;
-		color: white;
-		width: 100%;
-		height: 100%;
-	}
-	.mynav>li>a>span{
-		font-size: 0.9em;
-	}
-	.mynav>li>a:hover{
-		color:black;
-		font-weight: bold;
-		background: transparent;
-		border:0px;
-	}
-	.mynav>li>a:focus{
-		background:transparent;
-		color: white;
-	}
-	#li-x{
-		width: 4em;
-	}
-	#li-x:hover{
-		background: transparent;
-	}
-	#li-x a img{
-		height: 1.2em;
-	}
-</style>
-<nav class="navbar navbar-fixed-top mynavdiv">
-	<div <?php echo choose_class();?>>
-		<ul class="nav nav-tabs mynav">
-		  	<li role="presentation" id="li-x"><a href="javascript:void(0);"><img src="/xhust/thinkphp/Public/Apps/xhust.ico"/></a></li>
-	   		<li role="presentation"><a href='<?php echo U("Home/Index/index");?>'><span class="glyphicon glyphicon-home" aria-hidden="true"></span> &nbsp;首页</a></li>
-
-		  	<li role="presentation"><a href="<?php echo U('Home/Index/index');?>" target="_self"><span class="glyphicon glyphicon-flag" aria-hidden="true"></span> &nbsp;博客</a></li>
-		</ul>
-	</div>
-</nav>
-<div <?php echo choose_class();?>><div class="my-container">
+</script><body style="background: #eeeeee;"><style type="text/css">.mynavdiv{padding: 0;margin: 0;}.mynav{background: #2A2730;margin-top:0em;padding:0.3em;width: 100%;}.mynav>li{width: 7em;padding: 0;margin: 0;}.mynav>li:hover{background:rgba(255,255,255,0.5);}.mynav>li>a{font-size: 1.2em;color: white;width: 100%;height: 100%;}.mynav>li>a>span{font-size: 0.9em;}.mynav>li>a:hover{color:black;font-weight: bold;background: transparent;border:0px;}.mynav>li>a:focus{background:transparent;color: white;}#li-x{width: 4em;}#li-x:hover{background: transparent;}#li-x a img{height: 1.2em;}</style><nav class="navbar navbar-fixed-top mynavdiv"><div <?php echo choose_class();?>><ul class="nav nav-tabs mynav"> <li role="presentation" id="li-x"><a href='<?php echo U("Home/Index/index");?>'><img src="/xhust/thinkphp/Public/Apps/xhust.ico"/></a></li> <li role="presentation"><a href='<?php echo U("Home/Index/index#my-app-position");?>'><span class="glyphicon glyphicon-home" aria-hidden="true"></span> &nbsp;应用</a></li> <li role="presentation"><a href="<?php echo U('Home/Index/index#my-blog-position');?>" target="_self"><span class="glyphicon glyphicon-flag" aria-hidden="true"></span> &nbsp;博客</a></li></ul></div></nav><div <?php echo choose_class();?>><div class="my-container">
 <style type="text/css">
 	td{
 		border-collapse: separate;
@@ -312,20 +254,15 @@
 	var width = 10;
 	var height = 22;
 	var tileHelper;
-
 	function initGame() {
 	    tileHelper = new Tiles(".div_tile", width, height, ".div_panel" ,function(score){
-	    	if(score>0)
-	    	{
-	    		var txt='请输入你的昵称，上传分数！';
-	    		window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.input,{
-					onOk:function(name){
-						if(name==''){
-							name='anonymous';
-						}
-						uploadScore(name,score);
-					}
-				});
+	    	if(score>=0)
+	    	{	    		
+			    var value = prompt('输入你的名字：', 'anonymous');  
+			    if(value == null||value==""){  
+			        value='anonymous'
+			    }
+			    uploadScore(name,score);
 	    	}
 	    });
 	    tileHelper.initTile();
@@ -434,8 +371,25 @@
 			if(data['status']==1){
 				showRank();
 			}else{
-				window.wxc.xcConfirm(data['info'], window.wxc.xcConfirm.typeEnum.info);
+				alert_alt('上传失败');
 			}			
 		});
 	}
-</script><?php if(($app_id) != "0"): ?><div><button class="btn btn-success thumb-up"> &nbsp;我觉得这个应用不错，我要点赞&nbsp;<i class="fa fa-thumbs-up fa-lg"></i><span class="love_times"><?php echo ($love_times); ?></span>&nbsp;</button></div><?php endif; ?><div class="comment"><?php echo W('Comment/index',array($app_id));?></div><style type="text/css">li,ul,ol{list-style: none;display: inline-block;}.my-footer{background: #eeeeee;}.copyright{font-size: 1.1em;margin-top: 0.5em;}.copyright-donation{padding:0;padding-left: 0.5em;}.copyright-donation img{height: 8em;}.copyright-donation h5{width: 100%;text-align: center;padding-bottom: 0;margin-bottom: 0;}.contact span{font-size: 1em;display: inline-block;padding: 0.5em;}.contact a{text-decoration: none;}.about{height: 12em;}.about div{padding-left:0.5em;line-height: 2em;}/*.copyright-toast {position: absolute;bottom: 0;}*/</style><div class="my-footer"><div class="row copyright"><div class="col-md-8"><div class="row"><div class="col-md-5"><h4><i class="fa fa-credit-card fa-1x"></i>&nbsp;小额捐赠&nbsp;|&nbsp;Donation</h4><ul class="copyright-donation"><li><img src="/xhust/thinkphp/Public/Apps/alipay.gif"/><h5>支付宝</h5></li>&nbsp;&nbsp;&nbsp;&nbsp;<li><img src="/xhust/thinkphp/Public/Apps/wechat.gif"/><h5>微信支付</h5></li></ul></div><div class="col-md-7 contact"><h4><i class="fa fa-address-book fa-1x"></i>&nbsp;联系我&nbsp;|&nbsp;Contact me</h4><span><i class="fa fa-envelope fa-1x"></i>&nbsp;电子邮件:</span><a href="mailto:<?php echo ($contact["email"]); ?>"><?php echo (C("contact.email")); ?></a><br/><span><i class="fa fa-weibo fa-1x"></i>&nbsp;新浪微博:</span><a href="<?php echo (C("contact.weibo")); ?>" target="_blank"><?php echo (C("contact.weibo")); ?></a><br/><span><i class="fa fa-rss-square fa-1x"></i>&nbsp;博客园:</span><a href="<?php echo (C("contact.cnblog")); ?>" target="_blank"><?php echo (C("contact.cnblog")); ?></a><br/><span><i class="fa fa-github fa-1x"></i>&nbsp;Github:</span><a href="<?php echo (C("contact.github")); ?>" target="_blank"><?php echo (C("contact.github")); ?></a><br/></div></div></div><div class="col-md-4 about"><h4><i class="fa fa-info-circle fa-1x"></i>&nbsp;关于&nbsp;|&nbsp;About</h4><div><?php echo (C("contact.about")); ?></div><div>Gratitude for Thinkphp, Github, Bootstarp, Fontawesome, jQuery.</div><div>Designed for a girl@magua</div><div class="copyright-toast"><?php echo (C("contact.copyright")); ?></div></div></div></div><script type="text/javascript">jQuery(document).ready(function($) {});</script></div></div></body><script type="text/javascript">var pt=$(".mynavdiv").height();$('.my-container').css('padding-top',pt);jQuery(document).ready(function($) {$(window).resize(function(event) {var pt=$(".mynavdiv").height();$('.my-container').css('padding-top',pt);});$(".thumb-up").click(function(event) {var url='<?php echo U("Home/Index/addHeart");?>';var appid='<?php echo ($app_id); ?>';$.post(url, {id: appid}, function(data, textStatus, xhr) {if(data['status']==1){$(".love_times").text(data['data']);}});});$(window).load(function() {document.addEventListener('touchstart',function (event) { if(event.touches.length>1){ event.preventDefault(); } }); var lastTouchEnd=0; document.addEventListener('touchend',function (event) { var now=(new Date()).getTime(); if(now-lastTouchEnd<=300){ event.preventDefault(); } lastTouchEnd=now; },false); });});</script></html>
+</script><?php if(($app_id) != "0"): ?><div><button class="btn btn-success thumb-up"> &nbsp;我觉得这个应用不错，我要点赞&nbsp;<i class="fa fa-thumbs-up fa-lg"></i><span class="love_times"><?php echo ($love_times); ?></span>&nbsp;</button></div><?php endif; ?><div class="comment"><?php echo W('Comment/index',array($app_id));?></div><style type="text/css">li,ul,ol{list-style: none;display: inline-block;}.my-footer{background: #eeeeee;}.copyright{font-size: 1.1em;margin-top: 0.5em;}.copyright-donation{padding:0;padding-left: 0.5em;}.copyright-donation img{height: 8em;}.copyright-donation h5{width: 100%;text-align: center;padding-bottom: 0;margin-bottom: 0;}.contact span{font-size: 1em;display: inline-block;padding: 0.5em;}.contact a{text-decoration: none;}.about{height: 12em;}.about div{padding-left:0.5em;line-height: 2em;}/*.copyright-toast {position: absolute;bottom: 0;}*/</style><div class="my-footer"><div class="row copyright"><div class="col-md-8"><div class="row"><div class="col-md-5"><h4><i class="fa fa-credit-card fa-1x"></i>&nbsp;小额捐赠&nbsp;|&nbsp;Donation</h4><ul class="copyright-donation"><li><img src="/xhust/thinkphp/Public/Apps/alipay.gif"/><h5>支付宝</h5></li>&nbsp;&nbsp;&nbsp;&nbsp;<li><img src="/xhust/thinkphp/Public/Apps/wechat.gif"/><h5>微信支付</h5></li></ul></div><div class="col-md-7 contact"><h4><i class="fa fa-address-book fa-1x"></i>&nbsp;联系我&nbsp;|&nbsp;Contact me</h4><span><i class="fa fa-envelope fa-1x"></i>&nbsp;电子邮件:</span><a href="mailto:<?php echo ($contact["email"]); ?>"><?php echo (C("contact.email")); ?></a><br/><span><i class="fa fa-weibo fa-1x"></i>&nbsp;新浪微博:</span><a href="<?php echo (C("contact.weibo")); ?>" target="_blank"><?php echo (C("contact.weibo")); ?></a><br/><span><i class="fa fa-rss-square fa-1x"></i>&nbsp;博客园:</span><a href="<?php echo (C("contact.cnblog")); ?>" target="_blank"><?php echo (C("contact.cnblog")); ?></a><br/><span><i class="fa fa-github fa-1x"></i>&nbsp;Github:</span><a href="<?php echo (C("contact.github")); ?>" target="_blank"><?php echo (C("contact.github")); ?></a><br/></div></div></div><div class="col-md-4 about"><h4><i class="fa fa-info-circle fa-1x"></i>&nbsp;关于&nbsp;|&nbsp;About</h4><div><?php echo (C("contact.about")); ?></div><div>Gratitude for Thinkphp, Github, Bootstarp, Fontawesome, jQuery.</div><div>Designed for a girl@magua</div><div class="copyright-toast"><?php echo (C("contact.copyright")); ?></div></div></div></div><script type="text/javascript">jQuery(document).ready(function($) {});</script></div></div><style type="text/css">
+	.anchor{
+		position: fixed;
+		bottom: 1em;
+		right: 1em;
+	}
+	.anchor>a{
+		font-size: 2em;
+		color: #5BC0DE;
+	}
+	.anchor>a:hover{
+		font-size: 2em;
+		color: rgba(91,192,222,0.5);
+	}
+</style>
+<div class="anchor">
+	<a href="#"><i class="fa fa-arrow-circle-up fa-lg"></i></a>
+</div></body><script type="text/javascript">var pt=$(".mynavdiv").height();$('.my-container').css('padding-top',pt);jQuery(document).ready(function($) {$(window).resize(function(event) {var pt=$(".mynavdiv").height();$('.my-container').css('padding-top',pt);});$(".thumb-up").click(function(event) {var url='<?php echo U("Home/Index/addHeart");?>';var appid='<?php echo ($app_id); ?>';$.post(url, {id: appid}, function(data, textStatus, xhr) {if(data['status']==1){$(".love_times").text(data['data']);}});});$(window).load(function() {document.addEventListener('touchstart',function (event) { if(event.touches.length>1){ event.preventDefault(); } }); var lastTouchEnd=0; document.addEventListener('touchend',function (event) { var now=(new Date()).getTime(); if(now-lastTouchEnd<=300){ event.preventDefault(); } lastTouchEnd=now; },false); });});</script></html>
--- thinkphp/Application/Runtime/Cache/Apps/9bada13f8d65fe468b3b3610ce95bf98.php
+++ thinkphp/Application/Runtime/Cache/Apps/9bada13f8d65fe468b3b3610ce95bf98.php
@@ -250,7 +250,7 @@
 				<?php echo W('Comment/showComment',array(0,4,$app_id,$category));?>
 			</div>
 			<?php if(($page) > "0"): ?><div class="mybutton">
-					<?php $__FOR_START_9763__=0;$__FOR_END_9763__=$page;for($i=$__FOR_START_9763__;$i < $__FOR_END_9763__;$i+=1){ ?><button class="btn btn-default btn-page" id=<?php echo ($i); ?>><?php echo ($i+1); ?></button><?php } ?>			
+					<?php $__FOR_START_10933__=0;$__FOR_END_10933__=$page;for($i=$__FOR_START_10933__;$i < $__FOR_END_10933__;$i+=1){ ?><button class="btn btn-default btn-page" id=<?php echo ($i); ?>><?php echo ($i+1); ?></button><?php } ?>			
 				</div><?php endif; ?>
 		</div>
 		<div class="col-md-0">
